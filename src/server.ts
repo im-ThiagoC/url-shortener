40040f26777f54da152c5711dc18dc06a429a1d6
ThiagoC <thiagomedeiros82013@ufrrj.br>
2024-03-19 15:34:28 -0300

    Redirecionamento Funcionando

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,82 @@
-import fastify from "fastify";
+import fastify from "fastify"
+import { z } from "zod"
+import { sql } from './lib/postgres'
+import postgres from "postgres"
 
 const app = fastify()
 
-app.get('/teste', () => {
-    return 'Hello World'
+// GET, POST, PUT, DELETE, PATCH
+
+app.get('/api/links', async () => {
+    const result = await sql/*sql*/`
+        SELECT *
+        FROM urlshortener
+        ORDER BY created_at DESC
+    `
+
+    return result
+})
+
+app.get('/:code', async (request, reply) => {
+
+    const getLinkSchema = z.object({
+        code: z.string().min(3),
+    })
+
+    const { code } = getLinkSchema.parse(request.params)
+ 
+    const link = await sql/*sql*/`
+        SELECT id, original_url
+        FROM urlshortener
+        WHERE urlshortener.code = ${code}
+    `
+
+    if(!link[0]){
+        return reply.status(404).send({ error: 'The short link does not exist' })
+    }
+
+    // 301 = Moved Permanently
+    // 302 = Temporary Redirect
+    
+    return reply.redirect(301, link[0].original_url)
+})
+
+app.post('/api/links', async (request, reply) => {
+
+    const createLinkSchema = z.object({
+        code: z.string().min(3),
+        url: z.string().url(),
+    })
+
+
+    const { code, url } = createLinkSchema.parse(request.body)
+    
+
+    try{
+        const result = await sql/*sql*/`
+        INSERT INTO urlshortener (code, original_url)
+        VALUES (${code}, ${url})
+        RETURNING id
+    `
+
+        const link = result[0]
+
+        return reply.status(201).send({ urlShortenerId: link.id })
+
+    }
+    catch(err){
+        if(err instanceof postgres.PostgresError && err.code === '23505'){
+            return reply.status(400).send({ error: 'The short link already exists' })
+        }
+
+        console.error(err)
+
+        return reply.status(500).send({ error: 'Something went wrong' })
+    }
 })
 
 app.listen({
-    port: 3333,
+    port: 8000,
 }).then(() => {
-    console.log("Server is running on port 3333")
+    console.log("Server is running on port 8000")
 })
--- src/setup.ts
+++ src/setup.ts
@@ -4,9 +4,15 @@ async function setup() {
     await sql/*sql*/`
     CREATE TABLE IF NOT EXISTS urlshortener(
         id SERIAL PRIMARY KEY,
-        url TEXT NOT NULL,
+        code TEXT UNIQUE NOT NULL,
+        original_url TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     `
+
+    await sql.end()
+
+    console.log('Setup executado com sucesso!')
 }
 
 setup()
